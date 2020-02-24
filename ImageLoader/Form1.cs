dca4c93a66950d33f935bf7629e48578ffb9f234
ADarkHero <maik.riedlsperger@gmail.com>
2020-02-24 15:03:35 +0100

    checkAndAddEndingSlashes() doesn't add anything, if the fields are empty, now.

--- ImageLoader/Form1.cs
+++ ImageLoader/Form1.cs
@@ -152,11 +152,21 @@ namespace ImageLoader
 		/// </summary>
 		private void checkAndAddEndingSlashes()
 		{
-			if (!textBoxWebpath.Text.EndsWith("/"))
-				textBoxWebpath.Text = textBoxWebpath.Text + "/";
+			if (!textBoxWebpath.Text.EndsWith("/") && !String.IsNullOrEmpty(textBoxWebpath.Text))
+			{
+				Invoke(new Action(() =>
+				{
+					textBoxWebpath.Text = textBoxWebpath.Text + "/";
+				}));
+			}
 
-			if (!textBoxImageFolder.Text.EndsWith("\\"))
-				textBoxImageFolder.Text = textBoxImageFolder.Text + "\\";
+			if (!textBoxImageFolder.Text.EndsWith("\\") && !String.IsNullOrEmpty(textBoxImageFolder.Text))
+			{
+				Invoke(new Action(() =>
+				{
+					textBoxImageFolder.Text = textBoxImageFolder.Text + "\\";
+				}));
+			}
 		}
 
 		/// <summary>
